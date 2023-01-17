ea8fb422ce53dc524a57fcb27a1bdf75addaa645
Стојанов Давид <david.stojanov.1@students.finki.ukim.mk>
2023-01-17 21:26:38 +0100

    Initial commit & WebSecurityConfig

---
+++ src/main/java/dbva/bookzonelocal/BookZoneLocalApplication.java
@@ -0,0 +1,13 @@
+package dbva.bookzonelocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookZoneLocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookZoneLocalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dbva/bookzonelocal/BookZoneLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package dbva.bookzonelocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookZoneLocalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
